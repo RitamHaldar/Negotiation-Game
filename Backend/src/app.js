fb1d4ce2cf407c6fafa8529746b0d12d6440c69b
Ritam Haldar <rickhaldar561@gmail.com>
2026-03-28 18:34:25 +0530

    feat: Task Completed.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://negotiation-game.onrender.com",
     credentials: true
 }))
 
